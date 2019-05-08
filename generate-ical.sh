b845dfa512a75772a57626c0293650c3755fe296
Romain Pellerin <rpellerin@users.noreply.github.com>
2019-05-08 16:54:21 +0200

    Add generate-ical.sh

---
+++ generate-ical.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm ical.ical
+node index.js > ical.ical
+status=$?
+
+if [ $status -eq 0 ]; then
+    git add ical.ical
+    git commit -m 'Generate ical' -n --allow-empty
+    git push origin ical
+else
+    echo "Generate-ical.sh failed" | mail -s "Generate-ical.sh failed" root
+fi
